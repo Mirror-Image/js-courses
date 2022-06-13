3b5068d54ddb1ae11dd8ce0e8ff82ca702cb4b6d
Maksim Borovik <borovik.maksim@gmail.com>
2022-06-13 19:10:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Numbers
+
+let number = 7.8;
+let number2 = Infinity;
+let number3 = -Infinity;
+let number4 = NaN;
+
+// String
+// let string = 'Hello World';
+let fullName = () => 'Brian Boyanger';
+let string = `Hello, ${fullName()}`; // Hello, Brian Boyanger
+console.log(string);
+
+let someString = 'String';
+console.log(someString[1]);
+someString[1] = 'w';
+``
