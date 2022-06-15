5a7e33c1bb1a8c9832c622c44cc52cbcc2a7bc0f
Maksim Borovik <borovik.maksim@gmail.com>
2022-06-15 19:56:14 +0300

    lesson-2: objects and big O

--- index.js
+++ index.js
@@ -1,18 +1,84 @@
-// Numbers
+let a = 77;
+let b = a;
 
-let number = 7.8;
-let number2 = Infinity;
-let number3 = -Infinity;
-let number4 = NaN;
+// console.log(a, b); // 77, 77
 
-// String
-// let string = 'Hello World';
-let fullName = () => 'Brian Boyanger';
-let string = `Hello, ${fullName()}`; // Hello, Brian Boyanger
-console.log(string);
+a = 5;
 
-let someString = 'String';
-console.log(someString[1]);
-someString[1] = 'w';
+// console.log(a, b); // 5, 77
 
-// make something
+// Ссылочные типы данных
+let obj1 = {
+    fullName: 'Nick McGillan',
+};
+
+let obj2 = obj1;
+
+// console.log(obj1, obj2); //
+
+obj1.fullName = 'Brian Boyenger';
+
+// console.log(obj1, obj2); //
+
+const mainObj = {
+    a: 'test',
+    b: {
+        c: 777
+    },
+}
+
+const shellowCopyObject = Object.assign({}, mainObj);
+
+// console.log(shellowCopyObject);
+
+mainObj.a = 'Hellow world';
+
+// console.log(mainObj);
+// console.log(shellowCopyObject);
+
+mainObj.b.c = 'google';
+
+// console.log(mainObj);
+// console.log(shellowCopyObject);
+
+// console.log(String({}));
+
+const customerList = [
+    { firstName: 'Nick', secondName: 'McGillan', age: 35, phoneNumber: '+1333777888' },
+    { firstName: 'Brian', secondName: 'Boyenger', age: 35, phoneNumber: '+1555666777' },
+];
+
+const mappedCustomerList = customerList.reduce((acc, item) => {
+    acc[item.firstName + item.secondName] = item;
+
+    return acc;
+}, {});
+
+// console.log(mappedCustomerList);
+
+const binarySearc = (array, serchedValue) => {
+    let start = 0;
+    let end = array.length;
+    let base = Math.floor((start + end) / 2);
+    let step = 0;
+
+    for (let i = 0;  i < array.length; i++) {
+        if (array[base] !== serchedValue) {
+            if (serchedValue < array[base]) {
+                end = base;
+            } else {
+                start = base;
+            }
+            base = Math.floor((start + end) / 2);
+            step++;
+        };
+
+        if (array[base] === serchedValue) return `Found ${serchedValue} in ${step} steps`;
+    }
+
+    return 'Nothing found';
+}
+
+const sortedArray = [1, 5, 6, 7, 8, 20, 25, 26, 30, 35, 40, 50];
+
+console.log(binarySearc(sortedArray, 20));
