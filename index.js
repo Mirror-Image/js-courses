d29b202cc8026a30560d20152a2341d278968a7b
Maksim Borovik <borovik.maksim@gmail.com>
2022-06-27 18:53:07 +0300

    my func

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-// Numbers
+const infiniteCurry = fn => {
+    const next = (...args) => {
+      return x => {
+        if (!x) {
+          return args.reduce((acc, a) => {
+            return fn.call(fn, acc, a)
+          }, { result: 0, isStop: false });
+        }
+        return next(...args, x);
+      };
+    };
+    return next();
+  };
+  
+const getSum = infiniteCurry((x, y) => {
+    if (x.isStop) {
+        return x.result;
+    } else if (!x.isStop) {
+        if (typeof y === 'string') {
+            x.isStop = true;
+            return x;
+        }
+        x.result = x.result + y;
+    }
 
-let number = 7.8;
-let number2 = Infinity;
-let number3 = -Infinity;
-let number4 = NaN;
-
-// String
-// let string = 'Hello World';
-let fullName = () => 'Brian Boyanger';
-let string = `Hello, ${fullName()}`; // Hello, Brian Boyanger
-console.log(string);
-
-let someString = 'String';
-console.log(someString[1]);
-someString[1] = 'w';
-
-// make something
+    return x;
+});
+console.log(getSum(1)('wow')(3)(4)(2)('wow')());
+console.log(getSum(1)(3)('wow')(4)(2)('wow')());
+console.log(getSum(1)(3)(4)('wow')(2)('wow')());
